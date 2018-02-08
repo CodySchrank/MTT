9af127301a4b37107fcefa10fb7dfd9e9581fb14
Cody Schrank <cody.schrank@gmail.com>
2018-02-07 20:37:33 -0800

    0.1.5: Fixed import duplication bug and added auto generation tag

--- Example/Resources/Main/Basic.cs
+++ Example/Resources/Main/Basic.cs
@@ -3,6 +3,7 @@ namespace Example.Resources.Main
     public class Basic
     {
         public KeyValuePair Info { get; set; }
+        public KeyValuePair Name { get; set; }
         public DateTime LastUpdated { get; set; }
     }
 }
