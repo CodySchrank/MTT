b125d596cf41e444f9b808550078225e6cedf079
Marco van Kimmenade <marco.vank@outlook.com>
2019-07-31 13:09:46 +0200

    Remove Comments and PreProcessorDirective also for Enums.

---
+++ Source/MTTRunner/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MTT;
+
+namespace MTTRunner
+{
+    class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Arguments should contain the ConvertDirectory and the WorkingDirectory");
+            }
+
+            var convertService = new ConvertService((logString, logArgs) => Console.WriteLine(logString, logArgs))
+            {
+                ConvertDirectory = args[0],
+                WorkingDirectory = args[1]
+            };
+            convertService.Execute();
+
+            Console.ReadLine();
+        }
+    }
+}
