6acd77a347ab1ba638507f86c99487f11b8cba82
Marco van Kimmenade <marco.vank@outlook.com>
2019-07-31 13:13:43 +0200

    Add example for Comment and PreProcessorDirective in an Enum.

--- Example/Resources/Vehicles/VehicleState.cs
+++ Example/Resources/Vehicles/VehicleState.cs
@@ -1,11 +1,22 @@
 namespace Example.Resources.Vehicles
 {
-    public enum VehicleState: int 
+    public enum VehicleState : int
     {
+#if false
+#endif
+        /// <summary>
+        /// The broken state
+        /// </summary>
         Broken = 1,
 
+        /// <summary>
+        /// The used state
+        /// </summary>
         Used,
 
+        /// <summary>
+        /// The new state
+        /// </summary>
         New
     }
 }
