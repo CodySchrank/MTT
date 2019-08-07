d710d92fe177e8cf5253fb9745934f60850c48c2
CodySchrank <Cody.Schrank@gmail.com>
2019-08-07 16:04:48 -0700

    basic unit tests

--- Source/MTTRunner.Tests/UnitTest.cs
+++ Source/MTTRunner.Tests/UnitTest.cs
@@ -1,22 +1,90 @@
 using NUnit.Framework;
 using MTTRunner;
+using System.IO;
+using System;
 
-namespace Tests
+namespace MTTRunner.Tests
 {
-    public class Tests
+    public class BasicTests
     {
+        private readonly string CurrentDir = Directory.GetCurrentDirectory();
+        private readonly string WorkingDir = "workingDir/";
+        private readonly string ConvertDir = "convertDir/";
+
         [SetUp]
         public void Setup()
         {
-            var dirs = new string[] {"./ts", "./csharp"};
+            var resources = CurrentDir.Replace("\\", "/").Replace("Source/MTTRunner.Tests/bin/Debug/netcoreapp2.2", "example/Resources");
+
+            if(Directory.Exists(WorkingDir)) {
+                Directory.Delete(WorkingDir, true);
+            }
+
+            DirectoryCopy(resources, WorkingDir, true);
+
+            var dirs = new string[] {WorkingDir, ConvertDir};
 
             MTTRunner.Program.StartService(dirs);
         }
 
         [Test]
-        public void Test1()
+        public void WorkingDirExists()
         {
-            Assert.Pass();
+            if(Directory.Exists(Path.Combine(CurrentDir, WorkingDir))) {
+                Assert.Pass();
+            } else {
+                Assert.Fail();
+            }
+
+        }
+
+        [Test]
+        public void ConvertDirExists()
+        {
+            if(Directory.Exists(Path.Combine(CurrentDir, ConvertDir))) {
+                Assert.Pass();
+            } else {
+                Assert.Fail();
+            }
+        }
+
+
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+            
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                }
+            }
         }
     }
 }
